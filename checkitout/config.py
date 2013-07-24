180f3e255d57d0dabd9d26c08ebcf4548e5b5925
Philip Schleihauf <uniphil@gmail.com>
2013-07-24 11:21:03 -0400

    cooler config

--- checkitout/config.py
+++ checkitout/config.py
@@ -1,6 +1,6 @@
 from . import app
 
 app.config.update(
-    REPO_PATH='/home/phil/Code/calama-kale',
+    SQLALCHEMY_DATABAE_URI='sqlite://config.sqlite3',
     CHECKOUT_ROOT='/home/phil/checked-out/',
 )
