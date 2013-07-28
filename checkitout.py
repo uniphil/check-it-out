3dbdecb4f52d7da037766f246130a1174e3795c5
Philip Schleihauf <uniphil@gmail.com>
2013-07-27 20:43:35 -0400

    simplified structure wip

--- checkitout.py
+++ checkitout.py
@@ -7,21 +7,103 @@
 """
 
 from os import environ as env
+from requests import post
 from werkzeug import url_encode
-from github3 import login
-from flask import Flask, session, url_for, redirect, render_template
+from github3 import login as gh_from_login
+from flask import (Flask, request, session, url_for, redirect, render_template,
+                   flash, abort)
 from flask.ext.login import (LoginManager, login_required, UserMixin, 
-                             login_user, logout_user)
+                             login_user, logout_user, current_user)
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
+
 try:
     app.config.update(DEBUG=env.get('DEBUG', 'False')=='True',
-                      HOST=env['HOST'], PORT=env['PORT'])
+                      HOST=env['HOST'], PORT=env['PORT'],
+                      SECRET_KEY=env['SECRET_KEY'])
 except KeyError as e:
     e.message += "check-it-out must be configured with environment "\
-                 "variables.\n All are mandatory. See readme.md."
+                 "variables.\n See readme.md."
     raise e
 
+
+@app.route('/')
+def hello():
+    if not current_user.is_authenticated():
+        return render_template('hello.html')
+    return render_template('home.html')
+
+
+@app.route('/login')
+@login_manager.unauthorized_handler
+def login():
+    gh_auth_url = 'https://github.com/login/oauth/authorize'
+    redirect_uri = '{}:{}{}'.format(env.get('GH_CALLBACK_HOST', env['HOST']),
+                                    env.get('GH_CALLBACK_PORT', env['PORT']),
+                                    url_for('oauth_callback'))
+    auth_request_query = url_encode({'client_id': env['GH_CLIENT_ID'],
+                                     'redirect_uri': redirect_uri,
+                                     'scope': 'repo'})
+    app_auth_url = '{}?{}'.format(gh_auth_url, auth_request_query)
+    return redirect(app_auth_url)
+
+
+@app.route('/post-auth')
+def oauth_callback():
+    # Get the access token
+    gh_access_url = 'https://github.com/login/oauth/access_token'
+    access_request_data = dict(client_id=app.config['GH_CLIENT_ID'],
+                               client_secret=app.config['GH_CLIENT_SECRET'],
+                               code=request.args['code'])
+    gh_access = post(gh_access_url, data=access_request_data,
+                     headers={'accept': 'application/json'})
+    access_token = access_stuff.json().get('access_token')
+    assert access_token is not None, "no access token provided..."
+
+    # Now get YOU!!!
+    user = RepoUser.get(access_token)
+
+    # Get the repo...
+    repo_user, repo_name = env['APP_REPO'].split('/', 1)
+    gh_repo = user.gh.repository(repo_user, repo_name)
+    if gh_repo is None:
+        flash("Can\'t find that repo on GitHub for you. Do you have access?")
+        abort(404)
+
+    # Check access...
+    if not gh_repo.is_collaborator(gh_user.login):
+        abort(403)  # :(
+
+    # All good!
+    login_user(user, remember=True)
+    return redirect(url_for('hello'))
+
+
+class RepoUser(UserMixin):
+    """A user on GitHub with collaborator access to our repo."""
+    def __init__(self, gh):
+        self.gh = gh
+        self._gh_user = gh.user()
+
+    def __getattr__(self, name):
+        """Defer stuff we don't have to our github3 user instance."""
+        return getattr(self._gh_user, name)
+
+    @classmethod
+    def get(cls, access_code):
+        gh = gh_from_login(token=access_token)
+        return cls(gh)
+
+
+@app.errorhandler(403)
+def sadface(error):
+    return render_template('403-unauthorized.html'), 403
+
+
+@app.errorhandler(401)
+def ohnoes(error):
+    return render_template('404-whaaaaat.html'), 404
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -5,17 +5,25 @@ python wsgi app. and git. and together. and github. and stuff.
 
 ## Installation
 
-Configuration is via environment variables. You need to export the following:
 
- * `HOST`: hostname or IP for check-it-out to run on, like 'localhost' or `0.0.0.0`.
- * `PORT`: the port to listen on, like `80` or `5000`.
+Configuration is via environment variables.
+
+## Mandatory Configuration
+
+ * `HOST`: hostname or IP for check-it-out to run on, like 'localhost' or `0.0.0.0`. `check-it-out` will attempt to run at this hostname, and will also build the callback URL it passes to github. The callback url host can be overridden in case you are behind a reverse-proxy or something, see below the optional configuration section below.
+ * `PORT`: the port to listen on, like `80` or `5000`. Same things apply as with `HOST`.
  * `APP_REPO`: the github repo of the app you want, like `uniphil/check-it-out`.
  * `APP_ROOT`: check-it-out will download your wsgi apps inside this directory.
  * `GH_CLIENT_ID`: [from github](https://github.com/settings/applications), for user authentication.
  * `GH_CLIENT_SECRET`: also [from github](https://github.com/settings/applications).
  * `SECRET_KEY`: a [secret key for flask](http://flask.pocoo.org/docs/api/#flask.Flask.secret_key) that check-it-out needs to track sessions.
 
-`DEBUG` is the only optional environment variable, and can be `True` or `False`.
+## Optionl Configuration
+
+ * `DEBUG` will enable the flask/werkzeug debugger if set to `True`.
+ * `GH_OAUTH_CALLBACK_HOST` the public-facing host or IP for your `check-it-out` installation, which must match what you set up in your application registration [at github](https://github.com/settings/applications). `check-it-out` will use this to build the callback URL it passes to github, which is useful if you are behind a reverse-proxy or something.
+ * `GH_OAUTH_CALLBACK_PORT` same as above
+
 
 Tip: If you use [virtualenvwrapper](http://virtualenvwrapper.readthedocs.org/en/latest/), you can set up environment variable exports in the `postactivate` hook at `$VIRTUAL_ENV/bin/postactivate`. Mine looks like this:
 
